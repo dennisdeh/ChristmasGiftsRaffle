3fd5743ff9790e9c8ffa701042c3c283949d5a90
deh <dennishansendeh@gmail.com>
2025-11-08 08:51:31 +0100

    beta+ version

--- app/main.py
+++ app/main.py
@@ -1,12 +1,10 @@
 import streamlit as st
-import pandas as pd
 import numpy as np
-
+import os
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 np.random.seed(0)
-np.random.randint(10000000)
 
-
-st.set_page_config(page_title="Julegaver 2025", layout="centered")
+st.set_page_config(page_title="Julegaver 2025", layout="centered", page_icon="🎅")
 
 # Simple mapping dict
 d = {
@@ -25,17 +23,27 @@ np.random.shuffle(ls_out)
 d_out = dict(zip(d.keys(),ls_out))
 
 st.title("Indtast dit 8‑ciffer PIN for at se hvem du skal købe gaver til!")
-st.markdown("Ho-ho-ho")
+st.markdown("""
+Her er et lille ChatGPT-genereret juledigt til at varme op på:\n
+Under træet lå en pakke, rund og lidt for tung,
+nissen grined’ listigt, "det dér blir’ nok en gong!"
+Rudolf snublede i sneen, tabte hele læsset,
+og julemanden råbte: "Åh nej, hvor er min nissehuesnæsse!"
+Men julen kom alligevel – med grin og klejneræsset!\n
+Ho-ho-ho
+""")
 
-pin_input = st.text_input("PIN (8 chiffre)", max_chars=8, help="Indtast kun tal, fx 12345678")
+pin_input = st.text_input("Indtast din PIN (8 chiffre)", max_chars=8, help="Indtast kun tal, fx 12345678")
 
 # Validate and display result
-if st.button("Send"):
+if st.button("Send for at se hvem du skal købe til"):
     if not pin_input.isdigit() or len(pin_input) != 8:
-        st.error("Du skal indtast et 8-cifre PIN.")
+        st.error("PIN-koden du har fået er 8 chiffre")
     else:
         pin = str(pin_input)
         if pin in d:
-            st.success(d_out[pin])
+            st.success(f"Du skal give en gave til: {d_out[pin]}")
         else:
-            st.error("PIN kode ikke fundet, check igen eller ring til Dennis")
+            st.error("PIN kode ikke fundet, check igen - eller ring til Dennis")
+
+st.image(os.path.join(BASE_DIR, "christmas_mood.png"))
